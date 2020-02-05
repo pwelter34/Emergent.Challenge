68db82aafb7251d5e611830b8e426296a4ad9173
Paul Welter <pwelter@loresoft.com>
2020-02-05 12:36:12 -0600

    remove bad links

--- publish.ps1
+++ publish.ps1
@@ -1,2 +1,3 @@
+& dotnet build Emergent.Challenge.sln --configuration Release 
 & dotnet publish Emergent.Challenge.sln --configuration Release --output artifacts
-Copy-Item -Path "artifacts\Emergent.Challenge.Web\dist\*" -Destination ".\docs\" -recurse -Force
+Copy-Item -Path ".\artifacts\Emergent.Challenge.Web\dist\*" -Destination ".\docs\" -recurse -Force
